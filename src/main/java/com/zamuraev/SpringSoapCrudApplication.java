364fbec259915948b50b2e261723152099a50cb1
zamuraev <zamuraev.sergey@gmail.com>
2022-03-17 10:57:50 +0200

    Initial commit

---
+++ src/main/java/com/zamuraev/SpringSoapCrudApplication.java
@@ -0,0 +1,13 @@
+package com.zamuraev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoapCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSoapCrudApplication.class, args);
+    }
+
+}
